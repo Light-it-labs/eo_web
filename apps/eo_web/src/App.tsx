a99ac84220859c1713ab3f7980a65c099fc2bc77
charly.garcia <carlos.garcia@lightit.io>
2023-09-01 12:31:32 -0300

    fix: import bad cancer id from env.ts file

--- apps/eo_web/src/App.tsx
+++ apps/eo_web/src/App.tsx
@@ -8,17 +8,30 @@ import "react-toastify/dist/ReactToastify.css";
 
 
 const queryClient = new QueryClient();
+type envs =
+  | "www.eo.care"
+  | "partner.eo.care"
+  | "eo-marketing-06cbaf66a5b1fbfeecb0ca9525.webflow.io"
+  | "eo-marketing.webflow.io";
+
+interface EnvironmentsConfigs {
+  API_URL: string;
+  API_LARAVEL: string;
+  PROFILE_ONE_ID: string;
+  PROFILE_TWO_ID: string;
+  ZUKO_SLUG_ID_PROCESS_START: string;
+  CANCER_PROFILING: string;
+  CANCER_USER_DATA: string;
+  CANCER_SURVEY_FORM: string;
+}
+
 declare global {
   interface Window {
     data: {
-      API_URL: string;
-      API_LARAVEL: string;
-      PROFILE_ONE_ID: string;
-      PROFILE_TWO_ID: string;
-      ZUKO_SLUG_ID_PROCESS_START: string;
-      CANCER_PROFILING: string;
-      CANCER_USER_DATA: string;
-      CANCER_SURVEY_FORM: string;
+      hosts: envs[];
+      isProduction: boolean;
+      environments: Record<envs, EnvironmentsConfigs>;
+      getEnv: (key: keyof EnvironmentsConfigs) => string | null;
     };
   }
 }
