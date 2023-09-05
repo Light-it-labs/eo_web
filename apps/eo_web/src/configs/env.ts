147d237c5cf546e95a9e8c144d1069a73969ccc0
cgarcia-lightit <117183993+cgarcia-lightit@users.noreply.github.com>
2023-09-05 11:01:28 -0300

    [FEATURE] Environments separations (#27)
    * fix: import bad cancer id from env.ts file
    
    * review: implement changes from code-reivew

--- apps/eo_web/src/configs/env.ts
+++ apps/eo_web/src/configs/env.ts
@@ -1,14 +1,17 @@
-export const PROFILE_ONE_ID = window.data.PROFILE_ONE_ID || 231014818128147;
-export const PROFILE_TWO_ID = window.data.PROFILE_TWO_ID || 231015554139147;
+export const PROFILE_ONE_ID =
+  window.data.getEnv("PROFILE_ONE_ID") || 231014818128147;
+export const PROFILE_TWO_ID =
+  window.data.getEnv("PROFILE_TWO_ID") || 231015554139147;
 export const ZUKO_SLUG_ID =
-  window.data.ZUKO_SLUG_ID_PROCESS_START || "4e9cc7ceea3e22fb";
+  window.data.getEnv("ZUKO_SLUG_ID_PROCESS_START") || "4e9cc7ceea3e22fb";
 
 export const CANCER_USER_PROFILE =
-  window.data.CANCER_USER_DATA || 232256418562659;
+  window.data.getEnv("CANCER_USER_DATA") || 232256418562659;
 export const CANCER_PROFILE_ID =
-  window.data.CANCER_PROFILING || 232434941796667;
+  window.data.getEnv("CANCER_PROFILING") || 232434941796667;
 export const CANCER_SURVEY_ID =
-  window.data.CANCER_SURVEY_FORM || 232395615249664;
+  window.data.getEnv("CANCER_SURVEY_FORM") || 232395615249664;
 
-export const API_URL = window.data.API_URL || "http://localhost:4200";
-export const API_LARAVEL = window.data.API_LARAVEL || "http://localhost";
+export const API_URL = window.data.getEnv("API_URL") || "http://localhost:4200";
+export const API_LARAVEL =
+  window.data.getEnv("API_LARAVEL") || "http://localhost";
