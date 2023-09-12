ca38ccd94a6354c2fa22726acce3a6f466d8b87e
cgarcia-lightit <117183993+cgarcia-lightit@users.noreply.github.com>
2023-09-12 17:52:53 -0300

    Bugfix/changes classes (#29)
    * bugfix: remove fixed and w-screen to w-full and h-screen to w-full
    
    * bugfix: add build and add nunito font-format to buttons

--- apps/eo_web/src/screens/Cancer/UserTypeSelectorDemo.tsx
+++ apps/eo_web/src/screens/Cancer/UserTypeSelectorDemo.tsx
@@ -18,20 +18,20 @@ export const UserTypeSelectorDemo = () => {
 
   return (
     <LayoutDefault>
-      <div className="fixed flex h-screen w-screen items-center justify-center bg-[#f8f6f3] bg-opacity-50">
+      <div className="flex h-full w-full items-center justify-center bg-[#f8f6f3] bg-opacity-50">
         <div className="relative w-3/4 bg-white px-[43px] py-[52px] md:w-[742px]">
           <Typography className="text-nunito text-lg font-normal">
             Which best describes yous? <span className="text-red-600">*</span>
           </Typography>
           <div className="mt-6 flex flex-row gap-5">
             <button
-              className="h-[41px] w-1/2 border border-solid border-[#a5c4ff] bg-[#a5c4ff] bg-opacity-10 px-[15px] py-[9px]	"
+              className="text-nunito h-[41px] w-1/2 border border-solid border-[#a5c4ff] bg-[#a5c4ff] bg-opacity-10 px-[15px] py-[9px]	"
               onClick={() => redirectForm("Patient")}
             >
               Patient
             </button>
             <button
-              className="h-[41px] w-1/2 border border-solid border-[#a5c4ff] bg-[#a5c4ff] bg-opacity-10 px-[15px] py-[9px]	"
+              className="text-nunito h-[41px] w-1/2 border border-solid border-[#a5c4ff] bg-[#a5c4ff] bg-opacity-10 px-[15px] py-[9px]	"
               onClick={() => redirectForm("Caregiver")}
             >
               Caretaker
