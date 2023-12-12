9fa791825b4407fb5f8253ea7c11f3193ed10fca
cgarcia-lightit <117183993+cgarcia-lightit@users.noreply.github.com>
2023-12-12 19:49:38 -0300

    [FEATURE][86dqqqw1h] Updates to Thank you page (#46)
    * feature: change icons from carrousel on thank you page, remove unused route, add new color to tailwind and rename some icons
    
    * feat: modify subscribe section

--- apps/eo_web/src/router/routes.tsx
+++ apps/eo_web/src/router/routes.tsx
@@ -31,7 +31,6 @@ export const ROUTES = {
   athleteSurveyThankYou: "athletes/survey-thank-you",
 
   // Senior PATH
-  seniorAccountSetup: "/senior/account-setup",
   seniorForm: "/senior/profiling",
   seniorSurvey: "/senior/survey",
   seniorSurveyThankYou: "/senior/survey-thank-you",
