20f87c8a679d069cb749282bde7f97b9baef1597
charly.garcia <carlos.garcia@lightit.io>
2024-01-31 15:28:20 -0300

    feat: add new flow to identify the origin of the users

--- apps/eo_web/src/stores/useProfilingStore.ts
+++ apps/eo_web/src/stores/useProfilingStore.ts
@@ -19,6 +19,8 @@ export interface Account {
 
 export type Type = "Patient" | "Caregiver" | null;
 
+export type Flows = "c_org" | "marketing_site" | "cancer_pilot";
+
 export interface ProfilingStore {
   symptoms: string[];
   channel: Channel | null;
@@ -29,6 +31,7 @@ export interface ProfilingStore {
   origin: string;
   experience: string;
   account: Account;
+  flow: Flows;
   setAccountData: (account: Account) => void;
   setChannel: (channel: Channel) => void;
   setType: (type: Type) => void;
@@ -39,6 +42,7 @@ export interface ProfilingStore {
   resetProfilingStore: () => void;
   setOrigin: (origin: string) => void;
   setExperience: (experience: string) => void;
+  setFlow: (flow: Flows) => void;
 }
 
 const defaultState = {
@@ -59,6 +63,7 @@ const defaultState = {
     agreeTermsAndConditions: false,
   },
   usePayment: true,
+  flow: "marketing-site" as Flows,
 };
 
 export const useProfilingStore = create<ProfilingStore>()(
@@ -93,6 +98,9 @@ export const useProfilingStore = create<ProfilingStore>()(
       },
       setExperience: (experience: string) => {
         set({ experience });
+      },
+      setFlow: (flow: Flows) => {
+        set({ flow });
       },
       ...defaultState,
     }),
