00b7d41b30bbb877821652d98ceee125014b6464
Matias Fernandez <matias.fernandez@lightit.io>
2024-04-09 16:14:42 -0300

    feat: send email when someone subscribes to EO

--- apps/eo_web/src/api/email.ts
+++ apps/eo_web/src/api/email.ts
@@ -1,4 +1,4 @@
-import { apiElixir } from "~/api/axios";
+import { apiElixir, apiLaravel } from "~/api/axios";
 
 export interface ResendEmailVerificationResponse {
   success: boolean;
@@ -7,6 +7,17 @@ export interface ResendEmailVerificationResponse {
 export const resendEmailVerification = async (email: string) => {
   return await apiElixir.post<ResendEmailVerificationResponse>(
     "/v2/profile/resend_confirmation_email",
+    { email },
+  );
+};
+
+export interface SubscribeToEoEmailPostResponse {
+  success: boolean;
+}
+
+export const SubscribeToEoEmailPost = async (email: string) => {
+  return await apiLaravel.post<SubscribeToEoEmailPostResponse>(
+    "/api/slack-notification/eo-subscription",
     { email },
   );
 };
--- apps/eo_web/src/components/EOInYourInbox.tsx
+++ apps/eo_web/src/components/EOInYourInbox.tsx
@@ -1,8 +1,26 @@
 import { Button, Typography } from '@eo/ui'
 import React, { useState } from 'react'
+import { SubscribeToEoEmailPost } from '~/api/email'
 
 export const EOInYourInbox = () => {
+  const [isLoading, setIsLoading] = useState(false)
   const [submitted, setSubmitted] = useState(false)
+  const [email, setEmail] = useState('')
+
+  const onButtonSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsLoading(true)
+
+    try {
+      await SubscribeToEoEmailPost(email);
+    } catch (error) {
+      console.error('Error submitting email:', error);
+    } finally {
+      setIsLoading(false)
+      setSubmitted(true)
+    }
+  };
+
   return (
     <section className="w-full bg-white px-6 py-12 md:px-[50px] md:py-[100px]">
       <div className="flex flex-col items-center">
@@ -17,15 +35,18 @@ export const EOInYourInbox = () => {
           special offers are all coming soon.
         </Typography>
         {!submitted ?
-          (<div className="mt-[30px] flex w-full flex-col justify-items-end gap-4 md:w-auto md:flex-row">
+          (<form onSubmit={(e) => { void onButtonSubmit(e) }} className="mt-[30px] flex w-full flex-col justify-items-end gap-4 md:w-auto md:flex-row">
             <input
               className="h-[49px] w-full rounded-[40px] border border-solid border-black bg-white py-3 pl-4 pr-2 text-black placeholder:text-gray-300 md:w-[327px]"
               placeholder="Enter your email..."
+              type='email'
+              value={email}
+              onChange={(e) => { setEmail(e.target.value) }}
             />
-            <Button variant="black" font="semiBold" onClick={() => setSubmitted(true)}>
+            <Button type='submit' variant="black" font="semiBold" disabled={isLoading || !email}>
               Subscribe
             </Button>
-          </div>
+          </form>
           ) : (
             <div className="mt-[30px] px-6 py-12 lg:px-32 lg:py-18 rounded-lg border-2 border-black">
               <Typography font="semiBold" className="text-center text-lg  ">
@@ -35,6 +56,6 @@ export const EOInYourInbox = () => {
             </div>
           )}
       </div>
-    </section>
+    </section >
   )
 }
