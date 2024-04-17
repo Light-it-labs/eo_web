5e0dc19c7db91953d8a7b111f0ccbde1626c5c27
Matias Fernandez <132606537+Mat0ias@users.noreply.github.com>
2024-04-17 14:00:46 -0300

    [FEATURE][86drmx95g] Send email when someone subscribes to EO (#113)
    * feat: send email when someone subscribes to EO
    
    * fix: implementing improvements

--- apps/eo_web/src/components/EOInYourInbox.tsx
+++ apps/eo_web/src/components/EOInYourInbox.tsx
@@ -1,8 +1,19 @@
 import { Button, Typography } from '@eo/ui'
 import React, { useState } from 'react'
+import { useMutation } from '@tanstack/react-query'
+import { subscribeToEoEmailPost } from '~/api/email'
+import { toast } from 'react-toastify'
 
 export const EOInYourInbox = () => {
   const [submitted, setSubmitted] = useState(false)
+  const [email, setEmail] = useState('')
+
+  const { mutate, isLoading } = useMutation({
+    mutationFn: subscribeToEoEmailPost,
+    onSuccess: () => setSubmitted(true),
+    onError: () => toast.error("Something went wrong, try again.")
+  });
+
   return (
     <section className="w-full bg-white px-6 py-12 md:px-[50px] md:py-[100px]">
       <div className="flex flex-col items-center">
@@ -17,15 +28,22 @@ export const EOInYourInbox = () => {
           special offers are all coming soon.
         </Typography>
         {!submitted ?
-          (<div className="mt-[30px] flex w-full flex-col justify-items-end gap-4 md:w-auto md:flex-row">
+          (<form onSubmit={
+            (e) => {
+              e.preventDefault()
+              mutate(email)
+            }} className="mt-[30px] flex w-full flex-col justify-items-end gap-4 md:w-auto md:flex-row">
             <input
               className="h-[49px] w-full rounded-[40px] border border-solid border-black bg-white py-3 pl-4 pr-2 text-black placeholder:text-gray-300 md:w-[327px]"
               placeholder="Enter your email..."
+              type='email'
+              value={email}
+              onChange={(e) => { setEmail(e.target.value) }}
             />
-            <Button variant="black" font="semiBold" onClick={() => setSubmitted(true)}>
+            <Button type='submit' variant="black" font="semiBold" disabled={isLoading || !email}>
               Subscribe
             </Button>
-          </div>
+          </form>
           ) : (
             <div className="mt-[30px] px-6 py-12 lg:px-32 lg:py-18 rounded-lg border-2 border-black">
               <Typography font="semiBold" className="text-center text-lg  ">
@@ -35,6 +53,6 @@ export const EOInYourInbox = () => {
             </div>
           )}
       </div>
-    </section>
+    </section >
   )
 }
