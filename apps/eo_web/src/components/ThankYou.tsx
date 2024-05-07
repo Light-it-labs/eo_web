136ad2acfd62985fe31163eaec66961522aa2925
LeoBLightIt <leonardo.benveniste@lightit.io>
2024-05-07 12:41:08 -0300

    remove unused import

--- apps/eo_web/src/components/ThankYou.tsx
+++ apps/eo_web/src/components/ThankYou.tsx
@@ -1,4 +1,4 @@
-import { type HTMLAttributes, type ReactNode } from "react";
+import { type HTMLAttributes } from "react";
 import { Typography } from "@eo/ui";
 import { AllDonePanel } from "./AllDonePanel";
 import { useMutation } from "@tanstack/react-query";
